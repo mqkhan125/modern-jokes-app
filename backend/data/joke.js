03f5539dc3cf07bd63a110f6a963e233fbe8fa7c
mqkhan125 <muhammadqasimkhan341@gmail.com>
2026-05-20 14:57:34 +0500

    Initial setup: frontend and backend structure

---
+++ backend/data/joke.js
@@ -0,0 +1,49 @@
+export const jokes = [
+  {
+    id: 1,
+    title: "The Debugging Engineer",
+    content:
+      "A software engineer said love is like a bug—you only notice it after deployment.",
+    category: "Technology",
+    language: "en",
+    tags: ["engineer", "coding"],
+    likes: 12,
+    dislikes: 1,
+    views: 120,
+    isFeatured: true,
+    difficulty: "clever",
+    createdAt: new Date(),
+  },
+
+  {
+    id: 2,
+    title: "Office Meeting",
+    content:
+      "The meeting could have been an email, but then how would we waste everyone's time together?",
+    category: "Workplace",
+    language: "en",
+    tags: ["office", "meeting"],
+    likes: 20,
+    dislikes: 2,
+    views: 210,
+    isFeatured: false,
+    difficulty: "light",
+    createdAt: new Date(),
+  },
+
+  {
+    id: 3,
+    title: "Frontend Developer Life",
+    content:
+      "Frontend developers spend 90% of their time fixing CSS and 10% pretending it was intentional.",
+    category: "Programming",
+    language: "en",
+    tags: ["frontend", "css", "developer"],
+    likes: 35,
+    dislikes: 3,
+    views: 450,
+    isFeatured: true,
+    difficulty: "medium",
+    createdAt: new Date(),
+  },
+];
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <div className='bg-red-600'>app</div>
+  )
+}
+
+export default App
